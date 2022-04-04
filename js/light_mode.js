acceba54597f5be070b93085115b57bd02490ab4
abane1 <abanesebiane@outlook.fr>
2022-04-04 14:50:49 +0200

    responsive

--- js/light_mode.js
+++ js/light_mode.js
@@ -3,14 +3,18 @@ document.addEventListener("DOMContentLoaded", () => {
     let btn = document.querySelector("header span");
 	let icon = document.querySelector("header img");
 	let body = document.querySelector("body");
-	
+	console.log( icon);
 	btn.addEventListener("click", () => {
 		if (btn.innerHTML === `<span class="material-icons md-48">light_mode</span>`){
 			btn.innerHTML = `<span class="material-icons md-48">dark_mode</span>`;
-			icon.innerHTML = `<img src="./asset/Sans titre-1(1).ico" alt="logo netflix">`;
+			icon.setAttribute("src", "./asset/netflix_official_logo_icon_168085.ico");
+
 		} else {
 			btn.innerHTML = `<span class="material-icons md-48">light_mode</span>`;
+			icon.setAttribute("src", "./asset/icon_light_mode.png");
+
 		}
 		body.classList.toggle("light");
 	});
+
 });
